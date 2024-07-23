3ad201c78a721c0a7068f870d0ee2904ea7c4b50
Harsh Chaudhary <harshnimadia@gmail.com>
2024-07-23 17:25:34 +0530

    layout 💢 404 Page - Updated 🙃

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
+import styles from "./NotFound.module.css";
 
 export default function pageNotFound() {
-  return (  
+  return (
     <div className="center h-screen flex items-center justify-center">
       <div className="text-center">
       <h2 className="text-3xl">404 | Page Not Found</h2>
-      <p>Go back to the <Link href="/" className="underline">homepage</Link></p>
+      <p>Go back to the <Link href="/flights" className="underline">homepage</Link></p>
     </div>
     </div>
   )
 }
-  
-
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="bg-primary-200 text-primary-800 text-2xl">Flew With Us</div>;
+  return <div className="bg-background text-primary-800 text-2xl">Flew With Us</div>;
 }
