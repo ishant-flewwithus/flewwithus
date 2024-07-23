2e7dda16f437c1317ac3a399a7991471ac155eb2
ishant-flewwithus <ishant.flewwithus@gmail.com>
2024-07-23 14:16:25 +0530

    Tailwind theme setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div className="bg-yellow-200 text-yellow-800 text-2xl">Hello</div>;
+  return <div className="bg-primary-200 text-primary-800 text-2xl">Flew With Us</div>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          50: "#CCCDE9",
+          100: "#BEBFE2",
+          200: "#A1A3D6",
+          300: "#8588CA",
+          400: "#696CBD",
+          500: "#4D51B0",
+          600: "#3C3F89",
+          700: "#2B2D62",
+          800: "#1A1B3B",
+          900: "#090914",
+          950: "#000000",
+        },
+        onprimary: "#FFFFFF",
+        textheading: "#222222", // Main eg title
+        textbody: "#979797", // Secondary eg desc,
+        background: "#EEF2F8",
+        disabled: "#ACB8C9",
       },
     },
   },
