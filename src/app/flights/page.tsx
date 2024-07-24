dc4fc2d4374117a16a2a4874da44bfd3a414c3a1
ishant-flewwithus <ishant.flewwithus@gmail.com>
2024-07-24 12:30:53 +0530

    Nav bar made responsive

--- src/app/flights/page.tsx
+++ src/app/flights/page.tsx
@@ -8,6 +8,14 @@ import RoundedButton from "@/components/RoundedButton";
 import Container from "@/components/Container";
 import Section from "@/components/Section";
 import RoundedButtonBase from "@/components/RoundedButtonBase";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineInfo } from "react-icons/md";
+import { IoIosHelpCircleOutline } from "react-icons/io";
+import { MdOutlinePrivacyTip } from "react-icons/md";
+import { MdChecklist } from "react-icons/md";
+import { TbArticle } from "react-icons/tb";
+import { MdOutlineSettings } from "react-icons/md";
+import { useState } from "react";
 
 const navLinks = [
   {
@@ -27,16 +35,49 @@ const navLinks = [
   },
 ];
 
-export default function FlightHome({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const navLinksMobile = [
+  {
+    title: "About us",
+    navigateUrl: "/",
+    icon: <GiHamburgerMenu />,
+  },
+  {
+    title: "Help",
+    navigateUrl: "/",
+    icon: <IoIosHelpCircleOutline />,
+  },
+  {
+    title: "Privacy policy",
+    navigateUrl: "/",
+    icon: <MdOutlinePrivacyTip />,
+  },
+  {
+    title: "Terms of service",
+    navigateUrl: "/",
+    icon: <MdChecklist />,
+  },
+  {
+    title: "Blog",
+    navigateUrl: "/",
+    icon: <TbArticle />,
+  },
+  {
+    title: "Privacy Settings",
+    navigateUrl: "/",
+    icon: <MdOutlineSettings />,
+  },
+];
+
+export default function FlightHome() {
+  const [showMobileNavMenu, setShowMobileNavMenu] = useState(false);
+
   const pathname = usePathname();
   return (
     <Section fullHeight={true} backgroundImageUrl="/flight_home_image.png">
+      {/* NAV BAR */}
       <div className="flex items-center justify-between p-4">
-        <div className="relative h-[55px] w-[186px]">
+        {/* LOGO */}
+        <div className="relative h-[45px] w-[150px] lg:h-[55px] lg:w-[186px]">
           <Image
             src="logo_white_icon_text.svg"
             alt="Flew With Us Logo"
@@ -44,7 +85,8 @@ export default function FlightHome({
           />
         </div>
 
-        <div className="flex items-center justify-between gap-4">
+        {/* LINKS */}
+        <div className="hidden items-center justify-between gap-4 lg:flex">
           {navLinks?.map((item, index) => (
             <RoundedButton
               key={index}
@@ -55,7 +97,9 @@ export default function FlightHome({
             />
           ))}
         </div>
-        <div>
+
+        {/* SETTINGS BUTTON */}
+        <div className="flex items-center gap-5">
           <RoundedButtonBase>
             <Image
               src="/emojione-v1_flag-for-india.png"
@@ -63,10 +107,32 @@ export default function FlightHome({
               height={20}
               alt="India"
             />
-            <span className="text-sm">IND | ENG | INR</span>
-            <span className="text-textbody">|</span>
-            <span className="text-sm font-bold">LOGIN</span>
+            <span className="text-xs lg:text-sm">IND | ENG | INR</span>
+            <span className="text-xs text-textbody lg:text-sm">|</span>
+            <span className="text-xs font-bold lg:text-sm">LOGIN</span>
           </RoundedButtonBase>
+          <div className="relative lg:hidden">
+            <div
+              onClick={() => setShowMobileNavMenu(true)}
+              className="cursor-pointer text-2xl text-onprimary hover:text-onprimary/90"
+            >
+              <GiHamburgerMenu />
+            </div>
+            {showMobileNavMenu && (
+              <div className="absolute right-0 w-[60vw] sm:w-[45vw] md:w-[30vw] rounded-b-xl rounded-l-xl bg-primary-500 p-4 shadow-xl">
+                {navLinksMobile.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex cursor-pointer items-center gap-3 px-1 py-2 text-onprimary"
+                    onClick={() => setShowMobileNavMenu(false)}
+                  >
+                    <div>{item.icon}</div>
+                    {item.title}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </Section>
--- src/components/RoundedButtonBase.tsx
+++ src/components/RoundedButtonBase.tsx
@@ -6,7 +6,7 @@ export default function RoundedButtonBase({
   children,
 }: RoundedButtonBaseProps) {
   return (
-    <div className="flex cursor-pointer items-center justify-between gap-2 rounded-full border border-onprimary bg-onprimary px-4 py-2 font-normal text-primary-500">
+    <div className="text-xs flex cursor-pointer items-center justify-between gap-2 rounded-full border border-onprimary bg-onprimary px-4 py-2 font-normal text-primary-500">
       {children}
     </div>
   );
