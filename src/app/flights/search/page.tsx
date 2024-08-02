5c7c5face99c236c9237a968d48005ef9e7d84dd
ishant-flewwithus <ishant.flewwithus@gmail.com>
2024-08-02 15:30:48 +0530

    Search page made responsive

--- src/app/flights/search/page.tsx
+++ src/app/flights/search/page.tsx
@@ -15,6 +15,8 @@ import EveningDisabledIcon from "@/assets/evening_disabled.svg";
 import NightEnabledIcon from "@/assets/night_enabled.svg";
 import NightDisabledIcon from "@/assets/night_disabled.svg";
 import Image from "next/image";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { IoFilterOutline } from "react-icons/io5";
 
 export default function FlightSearchPage() {
   const baggageFilterOptions = [
@@ -174,13 +176,125 @@ export default function FlightSearchPage() {
       to: "Karachi, Pakistan",
       price: "₹ 16,040",
     },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
+    {
+      title: "IndiGo",
+      code: "6E 477",
+      startTime: "21:30",
+      endTime: "12:30",
+      duration: "02h 30m",
+      nonStop: true,
+      from: "New Delhi, India",
+      to: "Karachi, Pakistan",
+      price: "₹ 16,040",
+    },
   ];
+
+  const [showFiltersOnMobile, setShowFiltersOnMobile] = useState(false);
 
   return (
     <Page headerChild={<FlightSearch />} overlapChildrenOverHeader={false}>
       <div className="grid grid-cols-12 gap-8">
         {/* LEFT COLUMN */}
-        <div className="col-span-3">
+        <div className={`${showFiltersOnMobile ? "fixed" : "hidden"}  bottom-0 left-0 right-0 top-0 z-[200] col-span-12 w-screen xl:w-auto overflow-y-auto bg-onprimary p-4 xl:col-span-3 xl:block`}>
           <Stack direction="vertical" gap={20}>
             {/* BAGGAGE FILTERS */}
             <FormBox title="Check-in Baggage Filter">
@@ -251,62 +365,116 @@ export default function FlightSearchPage() {
                 <FormInputCheckbox key={index} label={item.title} />
               ))}
             </FormBox>
+
+            <div>
+              <div
+                className="rounded-full bg-primary-500 px-4 py-2 text-center text-onprimary xl:hidden cursor-pointer"
+                onClick={() => setShowFiltersOnMobile(false)}
+              >
+                Apply
+              </div>
+            </div>
           </Stack>
         </div>
         {/* RIGHT COLUMN */}
-        <div className="col-span-9">
-          <div className="text-2xl">
-            Flights from New Delhi to Bengaluru{" "}
-            <span className="font-semibold">( 20+ Flight )</span>
-          </div>
-          <div>
-            {flightsDummyData?.map((item, index) => (
+        <div className="col-span-12 xl:col-span-9">
+          <div className="flex items-center justify-between">
+            <div className="text-2xl">
+              Flights from New Delhi to Bengaluru{" "}
+              <span className="font-semibold">( 20+ Flight )</span>
+            </div>
+            <div>
               <div
-                className="mt-4 flex items-center justify-between rounded-md bg-onprimary p-6 shadow-sm"
-                key={index}
+                className="flex h-12 w-12 cursor-pointer flex-col items-center justify-center rounded-full bg-onprimary p-4 shadow-lg xl:hidden"
+                onClick={() => setShowFiltersOnMobile(true)}
               >
-                <div className="flex gap-4">
-                  <div className="flex items-center">
-                    <Image
-                      src="/indigo_logo.png"
-                      width={50}
-                      height={50}
-                      alt="indigo"
-                    />
+                <div>
+                  <IoFilterOutline />
+                </div>
+                <div className="text-xs">Filter</div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-4 overflow-y-auto scrollbar-thin xl:h-[130vh]">
+            {flightsDummyData?.map((item, index) => (
+              <div key={index}>
+                <div className="mt-4 hidden items-center justify-between rounded-md bg-onprimary p-6 shadow-sm lg:flex">
+                  <div className="flex gap-4">
+                    <div className="flex items-center">
+                      <Image
+                        src="/indigo_logo.png"
+                        width={50}
+                        height={50}
+                        alt="indigo"
+                      />
+                    </div>
+                    <div>
+                      <div className="text-xl font-semibold">{item?.title}</div>
+                      <div>{item?.code}</div>
+                    </div>
                   </div>
+
                   <div>
-                    <div className="text-xl font-semibold">{item?.title}</div>
-                    <div>{item?.code}</div>
+                    <div className="text-xl font-semibold">
+                      {item?.startTime}
+                    </div>
+                    <div>{item?.from}</div>
+                  </div>
+
+                  <div>
+                    <div className="text-xl font-semibold">{item?.endTime}</div>
+                    <div>{item?.to}</div>
+                  </div>
+
+                  <div>
+                    <div className="border-b-2 border-b-primary-500 pb-2 text-xs">
+                      {item?.duration}
+                    </div>
+                    <div className="pt-2 text-xs">Non-Stop</div>
+                  </div>
+
+                  <div>
+                    <div className="text-xl font-semibold">{item?.price}</div>
+                    <div>per adult</div>
+                  </div>
+
+                  <div>
+                    <div className="cursor-pointer rounded-full bg-primary-500 px-6 py-1 text-onprimary hover:bg-primary-600">
+                      View Prices
+                    </div>
                   </div>
                 </div>
-
-                <div>
-                  <div className="text-xl font-semibold">{item?.startTime}</div>
-                  <div>{item?.from}</div>
-                </div>
-
-                <div>
-                  <div className="text-xl font-semibold">{item?.endTime}</div>
-                  <div>{item?.to}</div>
-                </div>
-
-                <div>
-                  <div className="text-xs border-b-2 border-b-primary-500 pb-2">
-                    {item?.duration}
+                <div className="mt-4 block rounded-md bg-onprimary p-6 shadow-sm lg:hidden">
+                  <div className="text-center text-2xl font-semibold">
+                    {item.title}
                   </div>
-                  <div className="text-xs pt-2">
-                    Non-Stop
+                  <div className="text-center text-sm font-medium">
+                    {item.code}
                   </div>
-                </div>
-
-                <div>
-                  <div className="text-xl font-semibold">{item?.price}</div>
-                  <div>per adult</div>
-                </div>
-
-                <div>
-                  <div className="cursor-pointer rounded-full bg-primary-500 px-6 py-1 text-onprimary hover:bg-primary-600">
-                    View Prices
+                  <div className="mt-6 flex items-center justify-between text-center">
+                    <div>
+                      <div className="text-lg font-bold">5:00 AM</div>
+                      <div className="font-medium">Delhi, India</div>
+                    </div>
+                    <div>
+                      <div className="border-b-2 border-b-green-500 pb-1 text-sm font-bold">
+                        0 h 55 m
+                      </div>
+                      <div className="pt-2 text-xs font-medium">1 STOP</div>
+                    </div>
+                    <div>
+                      <div className="text-lg font-bold">8:00 AM</div>
+                      <div className="font-medium">Jaipur, India</div>
+                    </div>
+                  </div>
+                  <div className="mt-4 grid cursor-pointer grid-cols-3 rounded-full bg-primary-500 px-4 py-2 text-onprimary">
+                    <div className="text-start font-semibold line-through opacity-65">
+                      ₹ 2,200
+                    </div>
+                    <div className="text-end font-semibold">₹ 2,200</div>
+                    <div className="flex items-center justify-end">
+                      <FaArrowRightLong size={20} />
+                    </div>
                   </div>
                 </div>
               </div>
