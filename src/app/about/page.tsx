8310e548f1afe2b5367506c3ea54693da02e283e
Harsh Chaudhary <harshnimadia@gmail.com>
2024-08-07 16:48:35 +0530

    🐹 udated about us - added personalised text content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,14 +19,14 @@ export default function page() {
         </div>
         <div className="flex flex-col justify-start mt-20">
             <Image src={"/vision_year24.png"} width={1200} height={500} alt="our vision logo"></Image>
-            <div className="mt-16 max-w-6xl">
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonoru (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.
+            <div className="mt-16 max-w-6xl text-lg">
+            Our vision for 2024 is to help every traveler explore the world effortlessly, ensuring that the joys and wonders of travel are accessible for generations to come. We aim to create seamless, enriching experiences that transcend borders and cultural barriers, leveraging cutting-edge technology and sustainable practices. By fostering a global community of explorers, we are committed to making travel more intuitive, inclusive, and impactful, so that every journey is not just a trip, but a meaningful adventure that leaves a positive legacy for future generations.
             </div>
         </div>
-        <div className="flex flex-col items-end mt-20">
+        <div className="flex flex-col items-end mt-20 mb-8">
             <Image src={"/vision_year25.png"} width={1200} height={500} alt="our mission logo"></Image>
-            <div className="mt-16 max-w-6xl">
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 o Finibus Bonorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem IpsumLorem ipsum dolor sit amet, comes from a line in section 1.10.32.
+            <div className="mt-16 max-w-6xl text-lg">
+            By 2025, we aim to ensure that every traveler can explore the world effortlessly, creating lasting memories for generations to come. Our focus is on developing innovative travel solutions that simplify journeys, making them more accessible, enjoyable, and sustainable. Embracing new technologies and fostering a spirit of global connectivity, we strive to break down barriers and enhance the travel experience for everyone. Our commitment is to provide seamless and meaningful adventures, ensuring that the joy of discovery and exploration continues to inspire and connect people across the globe for years to come.
             </div>
         </div>
     </Page>
