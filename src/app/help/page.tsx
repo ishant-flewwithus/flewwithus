a5d11f3b12748dce84d196ce9423959a6c587a52
Harsh Chaudhary <harshnimadia@gmail.com>
2024-08-09 12:29:46 +0530

    🎀 help page updated- Adding assets in grid (Clickble btn img & responsive)

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,7 +1,28 @@
+import Page from "@/components/app/Page";
 
-
-export default function Help() {
+export default function privacySettings() {
   return (
-    <div>page</div>
-  )
+    <Page headerChild={
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-center text-5xl text-onprimary my-10 font-light ">
+        What can we <span className="font-bold">help you</span> with today?
+        </div>
+      <div className="h-12 w-full flex flex-row gap-5 px-72">
+        <div className="w-full h-full bg-white rounded-full cursor-pointer">
+
+        </div>
+        <div className="h-full w-1/3 bg-primary-500 rounded-full border-2 border-white cursor-pointer flex justify-center items-center text-onprimary text-xl">
+        Search
+        </div>
+      </div>
+      </div>
+    } overlapChildrenOverHeader={true}>
+        <div className="flex justify-center mt-24">
+          <div className="text-3xl">
+            Browse articles by topic
+          </div>
+        </div>
+        
+    </Page>
+  );
 }
